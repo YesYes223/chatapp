43eac84ce37cfcc7c98495cc9ab4b78f778fcb7a
Marcus <dryzle3@gmail.com>
2022-06-27 18:05:51 +0300

    added html and css for index.ejs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require("http")
 const server = http.createServer(app)
 
 app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.render("index")
